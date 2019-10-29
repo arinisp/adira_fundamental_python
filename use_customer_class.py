b37c7f1414a24fe60711b3bbe8063bf8ed89aa65
Arini Sri Purbani <arinisp@gmail.com>
2019-10-29 14:22:37 +0700

    Fundamental_phase_3_Package

--- use_customer_class.py
+++ use_customer_class.py
@@ -1,3 +1,5 @@
+from database.customer import Customer
+
 PRIA, WANITA = range(2)  # tipe data enumerasi, konstanta sebaiknya huruf besar tp ga wajib hanya sebaiknya
 FIX, NON_FIX = range(2)
 
@@ -12,3 +14,7 @@ customer1 = Customer('Boni', PRIA, 3500000, True, FIX,  {
 })
 customer2 = Customer('Sari', WANITA, 5500000, False, FIX, {})
 customer3 = Customer('Eka', WANITA,10000000, True, NON_FIX, {})
+
+print(customer1)
+print(customer2)
+print(customer3)
